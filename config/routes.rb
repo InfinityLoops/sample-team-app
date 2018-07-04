9fb349f87d0bbbc2b87a5687f9a7588655f5ed06
sesuonap <tpanouses@gmail.com>
2018-07-03 19:20:59 -0500

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get '/pages' => 'pages#index'
     get '/stuffed_mangos' => 'stuffed_mangos#index'
+    get '/purpleelephants' => 'purpleelephants#index'
   end
 end
